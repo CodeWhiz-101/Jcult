7377c87cd34b0b143bb4ca911c5264742f2f153e
Rohith <Rohith.vs2909@gmail.com>
2026-02-16 10:15:15 +0530

    chore: force vercel rebuild

--- src/app/what-we-do/page.tsx
+++ src/app/what-we-do/page.tsx
@@ -30,7 +30,7 @@ export default function WhatWeDo() {
         />
 
         {/* Small blue text left, para with border right */}
-    <section className="py-10 md:py-14 lg:py-20 bg-[#f6f6f6]">
+    <section className="py-10 md:py-14 lg:py-20 bg-[#f6f6f6] relative z-10 md:-mt-14 lg:-mt-20">
   <div className="container-responsive">
     <div className="grid grid-cols-1 md:grid-cols-[220px_1fr] gap-12 md:gap-16 items-start">
 
